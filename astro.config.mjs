5892a62c46bd169f0dde0710da71608d9005459a
yujun.liu <yujun.liu@funplus.com>
2024-02-28 16:37:09 +0800

    feat✨: update rss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://fluffy-zabaione-bd8d47.netlify.app"
+});
