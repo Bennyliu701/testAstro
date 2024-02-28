e557d4b540911a6e0ea8df4e55f0b82755b03130
yujun.liu <yujun.liu@funplus.com>
2024-02-28 18:19:42 +0800

    feat✨: finished blog work

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fluffy-zabaione-bd8d47.netlify.app/blog/"
+  site: "https://fluffy-zabaione-bd8d47.netlify.app/blog/",
+  integrations: [preact()]
 });
